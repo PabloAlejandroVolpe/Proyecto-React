27f0852820b087e471c1a79d8c324128442af41d
Pablo Volpe <pablo.volpe03999@gmail.com>
2022-10-20 00:40:39 -0300

    PreEntrega1 VolpePablo

--- src/Componentes/Main.js
+++ src/Componentes/Main.js
@@ -2,10 +2,13 @@ import React from "react";
 import ItemListContainer from "./ItemListContainer";
 
 
+
 function Main () {
     return (
+        <main>
         <ItemListContainer texto="Producto"
                      descripcion="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id rerum ducimus pariatur illum distinctio totam repudiandae consequuntur aperiam doloremque ratione?"/>
+        </main>
     )
 }
 
